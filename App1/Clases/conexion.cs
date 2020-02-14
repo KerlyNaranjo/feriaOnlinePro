f14f153b5ecbe2edbed4007c5fd085ff1f4441cc
KerlyNaranjo <k.erly5@hotmail.com>
2020-02-13 23:47:02 -0500

    Version 4

---
+++ App1/Clases/conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App1.Clases
+{
+	public class conexion
+	{
+		public string Conexion()
+		{
+			// return @"DATA SOURCE = CARTERA-LP;INITIAL CATALOG=OPTICA;INTEGRATED SECURITY=TRUE ";
+		//	return @"DATA SOURCE = 192.168.10.204;INITIAL CATALOG=CARRITO;INTEGRATED User ID=sa; Password=AdminCoimpexa.;";
+
+			return @"server=192.168.10.204;Database=carrito;User id=sa;Password=AdminCoimpexa.;";
+			///"Data Source=192.168.0.1;Initial Catalog=master;User ID=sa;Password=TuContraseña;Application Name=MyApp")
+		}
+
+	}
+}
--- Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoginArtesanos.MvcApplication" Language="C#" %>
